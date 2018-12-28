7b9ce384b340bc70d126e08f3f9865f513937a42
lbwa <32980314+lbwa@users.noreply.github.com>
2018-12-28 10:02:07 +0800

    fix(quick sort): adjust comparison part
    based on variables name

--- quick-sort.ts
+++ quick-sort.ts
@@ -7,13 +7,13 @@ function quickSort (arr: any[]): any[] {
   // extract benchmark item from original array
   const benchmark = arr.splice(Math.floor(arr.length / 2), 1)[0]
 
-  // Comparison module
+  // Comparison part
   arr.forEach(item => {
-    item <= benchmark ? greater.push(item) : lesser.push(item)
+    item <= benchmark ? lesser.push(item) : greater.push(item)
   })
 
   // recursive calling
-  return quickSort(greater).concat(benchmark, quickSort(lesser))
+  return quickSort(lesser).concat(benchmark, quickSort(greater))
 }
 
 // https://en.wikipedia.org/wiki/Quicksort
