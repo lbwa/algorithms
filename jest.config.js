545c75bbea39776078aa921b6bf84d95c69c086f
lbwa <mr_lbw@outlook.com>
2019-08-07 16:53:56 +0800

    chore(test): remove unnecessary comments

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,19 @@
-// For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
-  // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
 
-  // An array of file extensions your modules use
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
 
-  // A preset that is used as a base for Jest's configuration
   preset: 'ts-jest',
 
-  // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>'],
 
-  // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-  // snapshotSerializers: [],
-
-  // The test environment that will be used for testing
   testEnvironment: 'node',
 
-  // The glob patterns Jest uses to detect test files
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: ['\\\\node_modules\\\\']
 }
