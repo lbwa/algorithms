0d7b8f359ad5b5bd03aa79d8e06c14a1bb3ef6e7
lbwa <mr_lbw@outlook.com>
2019-08-07 14:32:01 +0800

    test: use jest and unit test for queue

---
+++ data-structures/queue/__test__/queue.test.ts
@@ -0,0 +1,24 @@
+import Queue from '../index'
+
+const maxQueueLength = 9
+const queue = new Queue(maxQueueLength)
+
+describe('Queue', () => {
+  it('should create empty queue', () => {
+    expect(queue.queue.length).toEqual(0)
+  })
+
+  it('Should enqueue and keep max length', () => {
+    let counter = 100
+    while (--counter >= 90) {
+      queue.enqueue({ name: `${counter}` })
+    }
+    expect(queue.queue.length).toBe(maxQueueLength)
+  })
+
+  it('Should requeue when item exist, and keep max length', () => {
+    queue.enqueue({ name: '96' })
+    expect(queue.queue.length).toBe(maxQueueLength)
+    expect(queue.queue[queue.queue.length - 1]).toEqual({ name: '96' })
+  })
+})
