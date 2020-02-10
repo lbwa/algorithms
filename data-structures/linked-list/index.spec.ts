3f329d2de424a594fbc4d48d3e7e478e1163388d
lbwa <mr_lbw@outlook.com>
2020-02-10 22:24:38 +0800

    feat(linked-list): delete element

--- data-structures/linked-list/index.spec.ts
+++ data-structures/linked-list/index.spec.ts
@@ -57,4 +57,46 @@ describe('Singly linked list', () => {
     list.prepend(items[2])
     expect(list.head!.value).toEqual(items[2])
   })
+
+  it('delete a element from a empty linked list', () => {
+    const list = new LinkedList()
+    expect(list.delete({ index: 1 })).toBeNull()
+    expect(list.head).toBeNull()
+    expect(list.tail).toBeNull()
+  })
+
+  it('delete the only one element', () => {
+    const list = new LinkedList(1)
+    expect(list.delete(1)).toEqual(1)
+    expect(list.head).toBeNull()
+    expect(list.tail).toBeNull()
+  })
+
+  it('delete a head element', () => {
+    const items = [{ index: 1 }, 2, [3]]
+    const list = new LinkedList(items[0])
+    list.append(items[1])
+    list.append(items[2])
+    expect(list.delete(items[0])).toEqual(items[0])
+    expect(list.head!.value).toEqual(items[1])
+  })
+
+  it('delete a tail element', () => {
+    const items = [{ index: 1 }, 2, [3]]
+    const list = new LinkedList(items[0])
+    list.append(items[1])
+    list.append(items[2])
+    expect(list.delete(items[2])).toEqual(items[2])
+    expect(list.tail!.value).toEqual(items[1])
+  })
+
+  it('delete a normal element', () => {
+    const items = [{ index: 1 }, 2, [3]]
+    const list = new LinkedList(items[0])
+    list.append(items[1])
+    list.append(items[2])
+    expect(list.head!.next!.next!.value).toEqual(list.tail!.value)
+    expect(list.delete(items[1])).toEqual(items[1])
+    expect(list.head!.next!.value).toEqual(list.tail!.value)
+  })
 })
--- data-structures/linked-list/index.ts
+++ data-structures/linked-list/index.ts
@@ -15,16 +15,20 @@ function createNode<E>(value: E, next: LinkedListNode<E> | null = null) {
 export class LinkedList<E> {
   head: LinkedListNode<E> | null = null
   tail: LinkedListNode<E> | null = null
+  isEqual: (a: E, b: E) => boolean
 
-  constructor(element?: E) {
+  constructor(element?: E, isEqual = (a: E, b: E) => a === b) {
     if (isDef(element)) {
       this.head = this.tail = createNode(element)
     }
+    this.isEqual = isEqual
   }
 
   append(element: E) {
     const node = createNode(element)
+
     if (!isDef(this.head) || !isDef(this.tail)) {
+      // optional step: It also can be return directly without this.head/tail assignment
       this.head = this.tail = node
       return this
     }
@@ -36,7 +40,9 @@ export class LinkedList<E> {
 
   prepend(element: E) {
     const head = createNode(element)
+
     if (!isDef(this.head) || !isDef(this.tail)) {
+      // optional step: It also can be return directly without this.head/tail assignment
       this.head = this.tail = head
       return this
     }
@@ -44,5 +50,44 @@ export class LinkedList<E> {
     head.next = this.head
     this.head = head
     return this
+  }
+
+  delete(element: E) {
+    // handle empty linked list
+    if (!isDef(this.head) || !isDef(this.tail)) {
+      return null
+    }
+
+    let deleteNode = null
+
+    // release head reference if target is equal to head value
+    if (this.isEqual(this.head.value, element)) {
+      // handle single element linked list
+      if (this.head === this.tail) {
+        this.tail = null
+      }
+      deleteNode = this.head.value
+      this.head = this.head.next // null
+      return deleteNode
+    }
+
+    // start from 2nd element
+    let current: LinkedListNode<E> | null = this.head
+    while (current && current.next) {
+      if (this.isEqual(current.next.value, element)) {
+        deleteNode = current.next.value
+        current.next = current.next.next
+        break
+      }
+      // jump to next node
+      current = current.next
+    }
+
+    // set tail element if target is equal to tail value
+    if (this.isEqual(this.tail.value, element)) {
+      this.tail = current
+    }
+
+    return deleteNode
   }
 }
