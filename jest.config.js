f29082d4480545dd71e3a03b07ba92c5c241fef6
Bowen Liu <mr_lbw@outlook.com>
2020-05-17 00:25:47 +0800

    test: modify jest moduleNameMapper to match path alias

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
-// https://jestjs.io/docs/en/configuration.html
-
+/**
+ * SHOULD read https://jestjs.io/docs/en/configuration.html first
+ */
 module.exports = {
   coverageDirectory: 'coverage',
 
   coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
 
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+
+  // https://jestjs.io/docs/en/configuration#modulenamemapper-objectstring-string--arraystring
+  moduleNameMapper: {
+    'sorts/(.*)': '<rootDir>/sorts/$1',
+  },
 
   preset: 'ts-jest',
 
@@ -15,5 +21,5 @@ module.exports = {
 
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 
-  testPathIgnorePatterns: ['\\\\node_modules\\\\', '\\\\dist\\\\']
+  testPathIgnorePatterns: ['\\\\node_modules\\\\', '\\\\dist\\\\'],
 }
