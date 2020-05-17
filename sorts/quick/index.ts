e203cd0210a1603deb1f62adbe60fe23395b26e3
Bowen Liu <mr_lbw@outlook.com>
2020-05-17 22:51:04 +0800

    chore: comments for in-place quick sort

--- sorts/quick/index.ts
+++ sorts/quick/index.ts
@@ -2,6 +2,7 @@ import { exchange } from 'sorts/shared/utils'
 
 /**
  * 快速排序（原地排序）
+ * @type 分治排序算法
  * @see https://en.wikipedia.org/wiki/Quicksort
  * @see https://algs4.cs.princeton.edu/23quicksort/
  */
@@ -10,6 +11,11 @@ export class QuickSort<E> {
     this.sort(this.list, 0, this.list.length - 1)
   }
 
+  /**
+   * 快速排序本质上是分治排序算法，始终将待排序数组分成两个子数组进行各自原地排序。当
+   * 两个子数组各自排序，即表示原始数组得到排序。
+   * 快速排序的性能核心在于 **切分** 方案，切分的位置取决于数组的内容
+   */
   private sort(list: E[], low: number, high: number) {
     if (high <= low) return
     const j = this.partition(list, low, high)
@@ -18,9 +24,14 @@ export class QuickSort<E> {
     return list
   }
 
+  /**
+   * 借助快慢指针实现分区内排序（以升序为例）
+   * 1. 指定 a[high] 为切分元素，该元素最后被排序，不参与被递归的子数组排序
+   * 2. 慢指针一定是小于切分元素，当快指针小于基准项时，交换快慢指针项。使得小项向前
+   * 3. 该切分方案的核心在于始终将小项靠前，并通过不断递归子数组实现小项排序
+   */
   private partition(list: E[], low: number, high: number) {
     const pivot = list[high]
-    // 借助双指针中快慢指针，实现分区排序
     let i = low // i is slow pointer, j is fast pointer
     for (let j = low; j < high; j++) {
       if (this.comparator(list[j], pivot)) {
@@ -28,7 +39,10 @@ export class QuickSort<E> {
         i++
       }
     }
+    // 切分元素 a[high] 不参与后续 sort 方法中递归子数组的排序，故交换至 i 项
+    // 递归子数组排序时，仅有 a[0] ~ a[i-1] 和 a[i+1] ~ a[a.length - 1] 项排序
     exchange(list, i, high)
+    // 返回当前切分元素索引
     return i
   }
 }
