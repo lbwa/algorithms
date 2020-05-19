c612757ffd59df0070ace33dd1beaeb17b58233d
Bowen Liu <mr_lbw@outlook.com>
2020-05-19 23:43:27 +0800

    chore: comments for complete binary tree

--- data-structures/priority-queue/index.spec.ts
+++ data-structures/priority-queue/index.spec.ts
@@ -1,8 +1,12 @@
 import { PriorityQueue } from './index'
 import { isDef } from 'shared/utils'
 
-// 在一个用数组表示的二叉堆中，第 k 项的父节点为 k/2（向下取整），子节点为 2k 和 2k+1
-function verifyHeap(heap: unknown[]) {
+/**
+ * 当前堆是否是 `堆有序`，即所有父节点都大于其两个子节点。
+ * 在一个用数组表示的二叉堆中，第 k 项的父节点为 k/2（向下取整），子节点为 2k 和 2k+1
+ * @see https://algs4.cs.princeton.edu/24pq/
+ */
+function isHeapOrdered(heap: unknown[]) {
   return heap.every((el, index) => {
     if (index > 0 && isDef(heap[2 * index])) {
       return (el as number) > (heap[2 * index] as number)
@@ -40,7 +44,7 @@ describe('Priority Queue', () => {
     expect(queue.size).toBe(6)
     expect(queue.hightest).toBe(12)
     // every node should be greater than its all child nodes.
-    expect(verifyHeap(queue.heap)).toBeTruthy()
+    expect(isHeapOrdered(queue.heap)).toBeTruthy()
   })
 
   it('Should delete root node', () => {
@@ -48,7 +52,7 @@ describe('Priority Queue', () => {
     expect(queue.deleteHightest()).toBeUndefined()
     ;[5, 2, 4, 1, 7, 6, 3, 8].forEach((el) => queue.insert(el))
     expect(queue.deleteHightest()).toBe(8)
-    expect(verifyHeap(queue.heap)).toBeTruthy()
+    expect(isHeapOrdered(queue.heap)).toBeTruthy()
     expect(queue.size).toBe(7)
     expect(queue.hightest).toBe(7)
   })
