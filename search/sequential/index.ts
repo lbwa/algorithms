bb8e6154fa621484ec64e88e678483bd58cb57e0
Bowen Liu <mr_lbw@outlook.com>
2020-05-29 00:01:43 +0800

    feat(search): add sequential search

---
+++ search/sequential/index.ts
@@ -0,0 +1,60 @@
+import { SymbolTable } from 'data-structures/symbol-table'
+
+class LinkedListNode<K, V> {
+  constructor(
+    public key: K,
+    public value: V | null,
+    public next: LinkedListNode<K, V> | null
+  ) {}
+}
+
+/**
+ * 基于无序链表的顺序查找，本质上即为线性查找
+ * @see https://algs4.cs.princeton.edu/31elementary/index.php#3.1
+ */
+export class SequentialSearch<K, V> extends SymbolTable<K, V> {
+  constructor(private head: LinkedListNode<K, V>) {
+    super()
+  }
+
+  get size() {
+    let i = 0
+    let current: LinkedListNode<K, V> | null = this.head
+    while (current) {
+      i++
+      current = current.next
+    }
+    return i
+  }
+
+  get isEmpty() {
+    return this.size === 0
+  }
+
+  get(key: K) {
+    let current: LinkedListNode<K, V> | null = this.head
+    while (current) {
+      if (current.key === key) {
+        return current.value // 命中
+      }
+      current = current.next // 未命中
+    }
+
+    return null
+  }
+
+  put(key: K, value: V | null) {
+    let current: LinkedListNode<K, V> | null = this.head
+    while (current) {
+      if (current.key === key) {
+        // 命中时，更新节点
+        current.value = value
+        return
+      }
+      current = current.next
+    }
+
+    // 未命中时，新建头节点
+    this.head = new LinkedListNode(key, value, this.head)
+  }
+}
