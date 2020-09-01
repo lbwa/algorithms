c084f59602804b595b1fd5628b3f35fc70a9701d
Bowen Liu <mr_lbw@outlook.com>
2020-09-02 00:05:15 +0800

    feat: golang test matcher

--- shared/utils.go
+++ shared/utils.go
@@ -1,6 +1,18 @@
 package utils
 
+import (
+	"reflect"
+	"testing"
+)
+
 // Swap two items in the list
 func Swap(list *[]int, a, b int) {
 	(*list)[a], (*list)[b] = (*list)[b], (*list)[a]
+}
+
+// Expect is a unit test matcher
+func Expect(t *testing.T, got, want interface{}) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
 }
